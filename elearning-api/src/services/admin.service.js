0d2a12e4e98d02dd4f65b5a3a76e05d93365fb40
Alex Wang <helemiat@gmail.com>
2026-06-08 16:08:53 +0700

    fix(competency): import and export competency types CRUD in admin.service.js

--- elearning-api/src/services/admin.service.js
+++ elearning-api/src/services/admin.service.js
@@ -24,7 +24,7 @@ const { getDashboardStats, getAdvancedAnalytics } = require('./admin/admin.analy
 const { getAdminAnnouncements, createAnnouncement, updateAnnouncement, deleteAnnouncement, archiveAnnouncement, republishAnnouncement, getAnnouncementHistory } = require('./admin/admin.announcements');
 const { getInstructorPresets, createInstructorPreset, updateInstructorPreset, deleteInstructorPreset } = require('./admin/admin.instructors');
 const { getOrganizationPresets, createOrganizationPreset, updateOrganizationPreset, deleteOrganizationPreset } = require('./admin/admin.organizations');
-const { getCompetencies, getCompetencyTree, createCompetencyGroup, updateCompetencyGroup, deleteCompetencyGroup, createCompetencyCategory, updateCompetencyCategory, deleteCompetencyCategory, createCompetency, updateCompetency, deleteCompetency, importGbtCompetencies } = require('./admin/admin.competencies');
+const { getCompetencies, getCompetencyTree, createCompetencyGroup, updateCompetencyGroup, deleteCompetencyGroup, createCompetencyCategory, updateCompetencyCategory, deleteCompetencyCategory, createCompetency, updateCompetency, deleteCompetency, importGbtCompetencies, getCompetencyTypes, createCompetencyType, updateCompetencyType, deleteCompetencyType } = require('./admin/admin.competencies');
 
 const {
     courseInclude,
@@ -106,6 +106,10 @@ module.exports = {
     updateCompetency,
     deleteCompetency,
     importGbtCompetencies,
+    getCompetencyTypes,
+    createCompetencyType,
+    updateCompetencyType,
+    deleteCompetencyType,
     getAdminCourses: (user) => getAdminCourses(user),
     createCourse,
     updateCourse,
